acc14bcfea53c028f90dc6359f6e61d7c99a1f5b
hermanhel <hermanhelf@yeah.net>
2021-07-12 17:24:40 +0800

    Initial Commit

---
+++ Drillbox/DrillboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrillboxApp.swift
+//  Drillbox
+//
+//  Created by Linfeng  He on 2021/7/12.
+//
+
+import SwiftUI
+
+@main
+struct DrillboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
